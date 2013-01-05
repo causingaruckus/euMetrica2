f228f22ec8860813ff453120ca78298b669cba6d
Dmitri Boulanov <leapingsteamroller@gmail.com>
2013-01-05 11:57:27 -0500

    Added Bluetooth connectivity
    Signal (value) should be displayed in the TextView in the middle of the
    screen, after the device is connected and found and started (starts
    upon hitting record)

--- em2project/src/com/eumetrica/em2/PatientEKGMain.java
+++ em2project/src/com/eumetrica/em2/PatientEKGMain.java
@@ -1,20 +1,32 @@
 package com.eumetrica.em2;
 
+import com.neurosky.thinkgear.TGDevice;
+import com.neurosky.thinkgear.TGEegPower;
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class PatientEKGMain extends Activity {
 
 	Button recordButton;
-	BluetoothAdapter bluetoothAdapter;
-	
+	TextView ekgValue;
+	TGDevice tgDevice;
+	BluetoothAdapter btAdapter;
+	private final static int REQUEST_ENABLE_BT = 1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,36 +35,80 @@ public class PatientEKGMain extends Activity {
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		recordButton = (Button) findViewById(R.id.record_ekg_button);
 		recordButton.setOnClickListener(recordButtonHandler);
+		ekgValue = (TextView) findViewById(R.id.ekg_value);
 	}
-	
-	View.OnClickListener recordButtonHandler = new View.OnClickListener() {
-		public void onClick(View v) {
-			
-			//Record the signal here
-			
-			
+
+	private final Handler handler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case TGDevice.MSG_STATE_CHANGE:
+				switch (msg.arg1) {
+				case TGDevice.STATE_IDLE:
+					break;
+				case TGDevice.STATE_CONNECTING:
+					break;
+				case TGDevice.STATE_CONNECTED:
+					tgDevice.start();
+					break;
+				case TGDevice.STATE_DISCONNECTED:
+					break;
+				case TGDevice.STATE_NOT_FOUND:
+				case TGDevice.STATE_NOT_PAIRED:
+				default:
+					break;
+				}
+				break;
+			case TGDevice.MSG_POOR_SIGNAL:
+				Log.v("HelloEEG", "PoorSignal: " + msg.arg1);
+			case TGDevice.MSG_ATTENTION:
+				Log.v("HelloEEG", "Attention: " + msg.arg1);
+				break;
+			case TGDevice.MSG_RAW_DATA:
+				int rawValue = msg.arg1;
+				ekgValue.setText(rawValue);
+				break;
+			case TGDevice.MSG_EEG_POWER:
+				//TGEegsPower ep = (TGEegPower) msg.arg1;
+				//Log.v("HelloEEG", "Delta: " + ep.delta);
+			default:
+				break;
+			}
 		}
 	};
-	
+
+	View.OnClickListener recordButtonHandler = new View.OnClickListener() {
+		public void onClick(View v) {
+
+			tgDevice.connect(true);
+			// Record the signal here
+		}
+	};
+
 	public void onSwitchFlipped(View view) {
-	    // Is the switch in the connected state?
-	    boolean connected = ((ToggleButton) view).isChecked();
-	    
-	    if (connected) {
-	    	//Connect to Bluetooth
-//	        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-//	        if(bluetoothAdapter == null) {
-//	        	// Alert user that Bluetooth is not available
-//	        	Toast.makeText(this, "Bluetooth not available", Toast.LENGTH_LONG).show();
-//	        	finish();
-//	        	return;
-//	        }  
-	    } else {
-	        // Disconnect from Bluetooth
-	    }
+		// Is the switch in the connected state?
+		boolean wantToConnect = ((ToggleButton) view).isChecked();
+
+		if (wantToConnect) {
+			// Initializing the Bluetooth adapter
+			btAdapter = BluetoothAdapter.getDefaultAdapter();
+			if (!btAdapter.isEnabled()) {
+			    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+			}
+			if (btAdapter != null) {
+				tgDevice = new TGDevice(btAdapter, handler);
+			} else{
+				 // Alert user that Bluetooth is not available
+				 Toast.makeText(this, "Bluetooth not available", Toast.LENGTH_LONG).show();
+				 finish();
+				 return;
+			}
+		} else {
+			tgDevice.close();
+		}
 	}
-	
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
